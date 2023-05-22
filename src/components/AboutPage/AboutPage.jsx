71a4f0ad15b9aa412e556ae2fea43b70a9da20bb
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-22 12:19:55 -0500

    Adjusted line spacing within the AboutPage

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,18 +10,21 @@ function AboutPage() {
     <div className="container">
       <div>
         <h2>About EZEats:</h2>
+        
         <p>
           EZEats is a recipe application designed to simplify your meal planning
           and accommodate dietary restrictions such as food allergies and celiac
           disease. Upon signing up, users can easily add new recipes to their
           profile and organize them into categories, such as entrees or desserts.
         </p>
+        
         <p>
-        Once you have recipes added, you will be greeted with an initial view of
-        different categories which expand to display the recipes within each
-        category. You will also see your most cooked recipes, these are recipes
-        that you have completed the most often (top 5).
+          Once you have recipes added, you will be greeted with an initial view of
+          different categories which expand to display the recipes within each
+          category. You will also see your most cooked recipes, these are recipes
+          that you have completed the most often (top 5).
         </p>
+        
         <p>
           When you view an individual recipe, you will be able to add it to the
           weekly shopping list. There will be a shopping list view where you can
@@ -33,12 +36,14 @@ function AboutPage() {
           shopping list. In the example above, if the recipe requiring 2 whole
           onions was removed, the list will adjust to show only  ½ onion.
         </p>
+        
         <p>
           EZEats streamlines the process of planning meals, organizing recipes,
           and generating comprehensive shopping lists. Whether you have specific
           dietary needs or simply want a more efficient approach to meal prep,
           EZEats is your go-to recipe app.
         </p>
+        
       </div>
     </div>
   );
