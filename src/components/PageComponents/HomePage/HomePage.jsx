fecf43db9adc38b36bc8c87dac78e474819a0f75
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-22 21:12:52 -0500

    Started to build HomePage
    - moving over to a HomePage branch

--- src/components/PageComponents/HomePage/HomePage.jsx
+++ src/components/PageComponents/HomePage/HomePage.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
 export default function HomePage() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch({type: 'FETCH_RECIPES'});
+  }, [])
+
+  const recipes = useSelector(store => store.recipes)
+  const mostCooked = useSelector(store => store.mostCooked)
+  
+
   return (
     <h1>Inside the logged in Home Page view</h1>
   )
