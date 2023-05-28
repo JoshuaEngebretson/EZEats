68b7c43736e7c0f01d4de9804d927462b7b30748
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-27 20:26:38 -0500

    Added a time limit before able to click Completed Recipe button
    - Now only able to increase the # of times_cooked once every 10 seconds.

--- src/components/PageComponents/ViewRecipePage/ViewRecipeButtons/CompletedRecipeButton/CompletedRecipeButton.jsx
+++ src/components/PageComponents/ViewRecipePage/ViewRecipeButtons/CompletedRecipeButton/CompletedRecipeButton.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './CompletedRecipeButton.css'
 
 export default function CompletedRecipeButton() {
   const dispatch = useDispatch();
   const currentRecipe = useSelector(store => store.recipes.currentRecipe[0])
+  const [canClick, setCanClick] = useState(true);
 
   const increaseTimesCooked = () => {
-    dispatch({type:'INCREASE_TIMES_COOKED', payload: currentRecipe.id})
+    // Setting a limit on how often the Completed Recipe button can be clicked.
+    if (canClick) {
+      // If canClick is true, allow the dispatch
+      dispatch({type:'INCREASE_TIMES_COOKED', payload: currentRecipe.id})
+      // Then set canClick equal to false
+      setCanClick(false);
+      // Then start a countdown for 10 seconds before canClick is set back to true
+      setTimeout(() => {
+        setCanClick(true);
+      }, 10000) // 10 seconds is equal to 10,000 milliseconds
+    } else {
+      console.log('Please wait a bit longer before attempting to increase the prepared count');
+    }
+
   }
 
   return (
