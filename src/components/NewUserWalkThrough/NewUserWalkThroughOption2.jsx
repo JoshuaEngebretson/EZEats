e773714b0ea433116de3f30da27929e9eebf3716
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-10-02 17:40:08 -0500

    Created Step1WalkThrough
    - Adjusted NewUserWalkThroughOption2 to change which step is rendered based on local state
    
    - Added additional css stylings to NewUserWalkThrough
    	- Buttons should now have a hover effect
    		- hover is a slightly darker background-color and white colored text
    	- Updated "back" class to "prev"
    	- Added a btn-modal-walkthrough-small class
    	- Updated "next" class to have a background color of lightblue instead of lightgreen
    	- Added a "confirm" class that has the background color of lightgreen now
    
    - NewUserWalkThroughOption2.jsx changes
    	- Updated variable "show" to "showModalWalkThrough"

--- src/components/NewUserWalkThrough/NewUserWalkThroughOption2.jsx
+++ src/components/NewUserWalkThrough/NewUserWalkThroughOption2.jsx
@@ -1,24 +1,27 @@
-﻿import "./NewUserWalkThrough.css";
+﻿import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import Step1WalkThrough from "./WalkThroughSteps/Step1WalkThrough";
 
 export default function NewUserWalkThroughOption2() {
 	const dispatch = useDispatch();
 	const recipeCount = useSelector(
 		(store) => store.recipes.allRecipes.length
 	);
-	const show = useSelector((store) => store.user.show_walk_through);
+	const showModalWalkThrough = useSelector(
+		(store) => store.user.show_walk_through
+	);
 
 	const [showNewUserWalkThrough, setShowNewUserWalkThrough] =
-		useState(show);
+		useState(showModalWalkThrough);
+	const [currentStep, setCurrentStep] = useState(0); // Track the current walkthrough step
 
 	useEffect(() => {
-		// Set up ability to click escape key to close out of the modal
 		const handleKeyDown = (event) => {
 			if (event.key === "Escape") {
 				toggleModal();
 			}
 		};
+
 		if (showNewUserWalkThrough) {
 			document.addEventListener("keydown", handleKeyDown);
 		} else {
@@ -26,7 +29,6 @@ export default function NewUserWalkThroughOption2() {
 		}
 	}, [showNewUserWalkThrough]);
 
-	// If the modal is open, don't allow the user to scroll within the page behind
 	if (showNewUserWalkThrough) {
 		document.body.classList.add("active-modal");
 	} else {
@@ -34,31 +36,120 @@ export default function NewUserWalkThroughOption2() {
 	}
 
 	const toggleModal = () => {
-		// Flip to true if currently false
-		// OR
-		// Flip to false if currently true
 		setShowNewUserWalkThrough(!showNewUserWalkThrough);
 		resetNewUserWalkThrough();
+	};
+
+	const resetNewUserWalkThrough = () => {
+		dispatch({ type: "RESET_NEW_USER_WALK_THROUGH" });
+		setCurrentStep(0); // Reset the current step when resetting the walkthrough
+	};
+
+	const nextWalkThroughStep = () => {
+		dispatch({ type: "NEXT_WALK_THROUGH_STEP" });
+		setCurrentStep(currentStep + 1); // Increment the current step
+	};
+
+	const prevWalkThroughStep = () => {
+		dispatch({ type: "PREV_WALK_THROUGH_STEP" });
+		setCurrentStep(currentStep - 1); // Decrement the current step
+	};
+
+	const startWalkthrough = () => {
+		nextWalkThroughStep();
+		// toggleModal();
+		// toggleModal();
+	};
+
+	const removeWalkThrough = () => {
+		toggleModal();
+		dispatch({ type: "REMOVE_NEW_USER_WALK_THROUGH" });
+	};
+
+	// Render the appropriate walkthrough step based on the currentStep
+	const renderWalkthroughStep = () => {
+		switch (currentStep) {
+			case 0:
+				return (
+					<div>
+						<h2>Welcome to EZEats</h2>
+						<p>
+							You currently have <b>{recipeCount}</b>{" "}
+							{recipeOrRecipes} added to EZEats.
+						</p>
+						<p>
+							Would you like to have some default
+							recipes?
+						</p>
+						<p>Or a walkthrough on how to use EZEats?</p>
+					</div>
+				);
+			case 1:
+				return <Step1WalkThrough />;
+			default:
+				return null;
+		}
+	};
+
+	const renderWalkThroughButtons = () => {
+		switch (currentStep) {
+			case 0:
+				return (
+					<>
+						<button
+							className="deny btn-modal-walkthrough"
+							onClick={removeWalkThrough}
+						>
+							No
+						</button>
+						<button
+							className="confirm btn-modal-walkthrough"
+							onClick={startWalkthrough}
+						>
+							Yes
+						</button>
+					</>
+				);
+			case 1:
+				return (
+					<>
+						<button
+							className="deny btn-modal-walkthrough"
+							onClick={toggleModal}
+						>
+							Cancel
+						</button>
+						<button
+							className="next btn-modal-walkthrough"
+							onClick={nextWalkThroughStep}
+						>
+							Next
+						</button>
+					</>
+				);
+			default:
+				return (
+					<>
+						<button
+							className="prev btn-modal-walkthrough"
+							onClick={prevWalkThroughStep}
+						>
+							Previous
+						</button>
+						<button
+							className="next btn-modal-walkthrough"
+							onClick={nextWalkThroughStep}
+						>
+							Next
+						</button>
+					</>
+				);
+		}
 	};
 
 	// If the recipeCount is equal to 1, then return message stating "recipe"
 	// Otherwise return the plural "recipes"
 	const recipeOrRecipes = recipeCount === 1 ? "recipe" : "recipes";
-
-	const nextWalkThroughStep = () => {
-		dispatch({ type: "NEXT_WALK_THROUGH_STEP" });
-	};
-	const prevWalkThroughStep = () => {
-		dispatch({ type: "PREV_WALK_THROUGH_STEP" });
-	};
-	const resetNewUserWalkThrough = () => {
-		dispatch({ type: "RESET_NEW_USER_WALK_THROUGH" });
-	};
-
-	const startWalkthrough = () => {
-		nextWalkThroughStep();
-		// return <Step1WalkThrough />;
-	};
 
 	return (
 		<>
@@ -69,36 +160,11 @@ export default function NewUserWalkThroughOption2() {
 						className="walkthrough-overlay"
 					></div>
 					<div className="modal-walkthrough-content">
-						<div>
-							<h2>Welcome to EZEats</h2>
-							<p>
-								You currently have{" "}
-								<b>{recipeCount}</b> {recipeOrRecipes}{" "}
-								added to EZEats.
-							</p>
-							<p>
-								{" "}
-								Would you like to have some default
-								recipes and a walk through on how to
-								use EZEats?
-							</p>
-						</div>
+						{renderWalkthroughStep()}{" "}
+						{/* Render the appropriate step */}
 						<div className="walkthrough-btns">
-							<button
-								className="deny btn-modal-walkthrough"
-								onClick={prevWalkThroughStep}
-							>
-								No
-							</button>
-
-							<button
-								className="next btn-modal-walkthrough"
-								onClick={startWalkthrough}
-							>
-								Yes
-							</button>
+							{renderWalkThroughButtons()}
 						</div>
-
 						<button
 							className="close-modal"
 							onClick={toggleModal}
